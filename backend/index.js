3271763aba8f997b5576c49f8a093411f9575b3f
Harish <gharishkumar.2023@gmail.com>
2020-09-17 00:24:56 +0530

    [1.0][COST] Adding few REST endpoints.

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ var http = require('http');
 var log4js = require('log4js');
 var logger = log4js.getLogger()
 var mxw = require('./mxw/mxw-blockchain');
+var bodyParser = require('body-parser');
 
 
 // Server setup
@@ -13,6 +14,13 @@ app.options('*', cors())
 app.use(cors())
 var host = 'localhost'
 var port = 9999
+
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
 //Set the logging level
 logger.level = 'trace'
@@ -35,11 +43,12 @@ function getErrorMessage(field){
 // Setting up the Maxonrow blockchain
 mxw.connectToMaxonrowBlockchain()
 
-app.get('/digital-wallet/v1.0/auth', async function(req, res) {
+app.post('/digital-wallet/v1.0/auth', async function(req, res) {
+    logger.info(req.body.username)
     var walletId = req.body.username;
     var mnemonic = req.body.mnemonic;
 
-    if(!walledId){
+    if(!walletId){
         res.json(getErrorMessage('walletId'));
         return;
     }
@@ -47,4 +56,156 @@ app.get('/digital-wallet/v1.0/auth', async function(req, res) {
         res.json(getErrorMessage('mnemonic'));
         return;
     }
+
+    let message = await mxw.findWallet(mnemonic, walletId)
+    
+    res.statusCode = message.status;
+    res.send(message)
+});
+
+app.post('/digital-wallet/v1.0/createNft', async function(req, res) {
+    var symbol = req.body.symbol;
+    var name = req.body.name;
+    var properties = req.body.properties;
+    var metadata = req.body.metadata;
+    var feeCollector = req.body.feeCollector;
+    var endorserList = req.body.endorserList;
+    // Sanity checking.
+    if(!symbol)
+    {
+        res.json(getErrorMessage('symbol'));
+        return;
+    }
+    if(!name) 
+    {
+        res.json(getErrorMessage('name'));
+        return;
+    }
+    if(!properties) 
+    {
+        res.json(getErrorMessage('properties'));
+        return;
+    }
+    if(!metadata)
+    {
+        res.json(getErrorMessage('metadata'));
+        return;
+    }
+    if(!feeCollector)
+    {
+        res.json(getErrorMessage('feeCollector'));
+        return;
+    }
+    mxw.createNft(symbol, name, properties, metadata, feeCollector, res);    
+});
+
+app.post('/digital-wallet/v1.0/authorizeNft', async function(req, res) {
+    var provider = req.body.provider;
+    var middleWare = req.body.middleWare;
+    var endorserList = req.body.endorserList;
+    var mintLimit = req.body.mintLimit;
+    var transferLimit = req.body.transferLimit;
+    var burnable = req.body.burnable;
+    var pub = req.body.public;
+    var symbol = req.body.symbol;
+    if(!provider)
+    {
+        res.json(getErrorMessage('provider'));
+        return;
+    }
+    if(!middleWare)
+    {
+        res.json(getErrorMessage('middleWare'));
+        return;
+    }
+    if(!endorserList){
+        res.json(getErrorMessage('endorserList'));
+        return;
+    }
+    if(!mintLimit){
+        res.json(getErrorMessage('mintLimit'));
+        return;
+    }
+    if(!transferLimit){
+        res.json(getErrorMessage('transferLimit'));
+        return;
+    }
+    if(!burnable){ 
+        res.json(getErrorMessage('burnable'));
+        return;
+    }
+    if(!pub){
+        res.json(getErrorMessage('public'));
+        return;
+    }
+    if(!symbol){
+        res.json(getErrorMessage('symbol'));
+        return;
+    }
+
+    let message = await mxw.authorizeNft(provider, middleWare, endorserList, mintLimit, transferLimit, burnable, pub, symbol);
+    res.statusCode = message.status;
+    res.send(message);
+})
+
+app.post('/digital-wallet/v1.0/mintNft', async function(req, res) {
+    var symbol = req.body.symbol;
+    var itemId = req.body.itemId;
+    var properties = req.body.properties;
+    var metadata = req.body.metadata;
+    if(!symbol)
+    {
+        res.json(getErrorMessage('symbol'));
+        return;
+    }
+    if(!itemId)
+    {
+        res.json(getErrorMessage('itemId'));
+        return;
+    }
+    if(!properties)
+    {
+        res.json(getErrorMessage('properties'));
+        return;
+    }
+    if(!metadata)
+    {
+        res.json(getErrorMessage('metadata'));
+        return;
+    }
+
+    let message = await mxw.mintNftTokens(symbol, itemId, properties, metadata);
+    res.statusCode = message.status;
+    res.send(message);
+})
+
+app.post('/digital-wallet/v1.0/transferNft', async function(req, res) {
+    var walletId = req.body.walletId;
+    var symbol = req.body.symbol;
+    var itemId = req.body.itemId;
+    var issuerAddress = req.body.issuerAddress;
+    if(!symbol)
+    {
+        res.json(getErrorMessage('symbol'));
+        return;
+    }
+    if(!walletId)
+    {
+        res.json(getErrorMessage('walletId'));
+        return;
+    }
+    if(!itemId)
+    {
+        res.json(getErrorMessage('itemId'));
+        return;
+    }
+    if(!issuerAddress)
+    {
+        res.json(getErrorMessage('issuerAddress'));
+        return;
+    }
+
+    let message = await mxw.transferNftOwnership(walletId, symbol, itemId, issuerAddress);
+    res.statusCode = message.status;
+    res.send(message);
 })
--- backend/mxw/mxw-blockchain.js
+++ backend/mxw/mxw-blockchain.js
@@ -2,25 +2,212 @@
 var mxw =  require('mxw-sdk-js/dist');
 var network = require('../configuration/mxw-provider');
 var logger = require('log4js').getLogger()
+var utils = require('mxw-sdk-js/dist/utils')
+var nftToken = require('mxw-sdk-js/dist/index')
+var ex = require('mxw-sdk-js/dist/non-fungible-token')
+var nftTokenItem = require('mxw-sdk-js/dist/non-fungible-token-item')
+
 logger.level = 'trace'
 
 
 
 let issuer = mxw.Wallet;
+let provider = mxw.Wallet; 
+let middleware = mxw.Wallet;
 let nodeProvider = network.nodeProvider;
 let providerConnection;
 
 var connectToMaxonrowBlockchain = async function(){
-    logger.info('Connecting to Maxonrow Blockchain')
+    logger.info('Connecting to Maxonrow Blockchain');
     providerConnection = new mxw.providers.JsonRpcProvider(nodeProvider.connection, nodeProvider);
-    logger.info('Connected to Maxonrow Blockchain')
+    logger.info('Connected to Maxonrow Blockchain');
     intialiseWallets();
 }
 
 var intialiseWallets = async function() {
-    logger.info('Fetching the issuer wallet details')
-    issuer = mxw.Wallet.fromMnemonic(nodeProvider.nonFungibleToken.issuer).connect(providerConnection)
-    logger.info('Done fetching the issuer wallet details')
+    logger.info('Fetching the wallet details');
+    issuer = mxw.Wallet.fromMnemonic(nodeProvider.nonFungibleToken.issuer).connect(providerConnection);
+    provider = mxw.Wallet.fromMnemonic(nodeProvider.nonFungibleToken.provider).connect(providerConnection);
+    middleware = mxw.Wallet.fromMnemonic(nodeProvider.nonFungibleToken.middleware).connect(providerConnection);
+    logger.info('Done fetching the wallet details');
 }
 
+var findWallet = async function(mnemonic, walletId){
+    var res = {
+        success: true,
+        message: 'Successfully verified',
+        status: 200
+    };
+    let fullWallet = mxw.Wallet.fromMnemonic(mnemonic).connect(providerConnection);
+    if(walletId === fullWallet.address){
+        return res;
+    }
+
+    res.success = false;
+    res.message = 'The provided details are invalid'
+    res.status = 401;
+
+    return res;
+}
+
+
+
+var createNft = async function(symbol, name, properties, metadata, feeCollector, res){
+    
+    if(feeCollector === network.localnet.nonFungibleToken.feeCollector)
+    {
+        var nftProps = {
+            name: name,
+            symbol: symbol,
+            fee: {
+                to: feeCollector,
+                value: utils.bigNumberify("1")
+            },
+            endorserList: [],
+            endorserListLimit: 10,
+            properties: properties,
+            metadata: metadata
+        }
+        var token = new ex.NonFungibleToken(symbol, provider);
+        nftToken.nonFungibleToken.NonFungibleToken.create(nftProps, provider).then((token) => {
+            var resp = {
+                success: true,
+                status: 200,
+                message: 'Successfully created ' + symbol + ' token',
+                details: String(token)
+            };
+            res.statusCode = resp.status;
+            res.send(resp);
+        }).catch((error) => {
+            var resp = {
+                success: false,
+                status: 400,
+                message: 'Something went wrong',
+                details: String(error)
+            };
+            res.statuCode = resp.status;
+            res.send(resp);
+        })
+    }
+    else{
+        var resp = {
+            success: false,
+            status: 401,
+            message: 'You are not authorized to create a token'
+        }
+        return resp;
+    }
+}
+
+var authorizeNft = async function(providerAddress, middleWareAddress, endorserList, mintLimit, transferLimit, burable, pub, symbol) {
+    // Validate if the wallet details obtained are right.
+    var resp = {
+        success: true,
+        message: 'Successfully authorized.',
+        status: 200
+    }
+
+    if(providerAddress != provider.address) {
+        resp.success = false;
+        resp.message = 'Invalid provider address.';
+        resp.status = 401
+        return resp;
+    }
+    if(middleWareAddress != middleware.address) {
+        resp.success = false;
+        resp.message = 'Invalid provider address.';
+        resp.status = 401;
+        return resp;
+    }
+
+    var tokenState = {
+        tokenFees: [ 
+            {action: nftToken.nonFungibleToken.NonFungibleTokenActions.transfer, feeName: "default"},
+            {action: nftToken.nonFungibleToken.NonFungibleTokenActions.transferOwnership, feeName: "default"},
+            {action: nftToken.nonFungibleToken.NonFungibleTokenActions.acceptOwnership, feeName: "default"}
+        ],
+        endorserList: endorserList,
+        mintLimit: mintLimit,
+        transferLimit: transferLimit,
+        burnable: burable,
+        pub: pub
+    }
+
+    nftToken.nonFungibleToken.NonFungibleToken.approveNonFungibleToken(symbol, provider, tokenState).then((transaction) => {
+        nftToken.nonFungibleToken.NonFungibleToken.signNonFungibleTokenStatusTransaction(transaction, issuer).then((transaction) => {
+            nftToken.nonFungibleToken.NonFungibleToken.signNonFungibleTokenStatusTransaction(transaction, middleWare).then((receipt) => {
+                return resp;
+            }).catch((error) => {
+                resp.success = false;
+                resp.status = 400;
+                resp.message = String(error);
+                return resp;
+            })
+        }).catch((error) => {
+            resp.message = String(error);
+            resp.success = false;
+            resp.status = 400;
+            return resp;
+        })
+    }).catch((error) => {
+        resp.message = String(error);
+        resp.success = false;
+        res.status = 400;
+        return resp;
+    })    
+}
+
+var mintNftTokens = async function(symbol, itemId, properties, metadata) {
+    var resp = {
+        success: true,
+        message: 'Successfully authorized.',
+        status: 200
+    }
+    let mintToken = {
+        symbol: symbol,
+        itemId: itemId,
+        properties: properties,
+        metadata: metadata
+    }
+
+    let minter = new nftToken.nonFungibleToken.NonFungibleToken(symbol, issuer);
+    minter.mint(issuer.address, mintToken).then((transaction) => {
+        return(resp);
+    }).catch((error) => {
+        resp.status = 400;
+        resp.success = false;
+        resp.message = 'Something went wrong. Please check the details provided.';
+    })
+}
+
+var transferNftOwnership = async function(walletId, symbol, itemId, issuerAddress) {
+    var resp = {
+        success: true,
+        status: 200,
+        message: 'Transfer successful'
+    }
+    if(issuerAddress != issuer.address){
+        resp.success = false;
+        resp.status = 401;
+        resp.message = 'Invalid Issuer.'
+        return resp;
+    }
+
+    let nftItem = new nftTokenItem.NonFungibleTokenItem(symbol, itemId, issuer);
+    nftItem.transfer(walletId).then((transaction) => {
+        return resp;
+    }).catch((error) => {
+        resp.message = 'Unable to transfer Token' + String(error);
+        resp.status = 500;
+        resp.success = false;
+        return resp;
+    })
+}
+
+
+exports.transferNftOwnership = transferNftOwnership;
+exports.mintNftTokens = mintNftTokens;
+exports.authorizeNft = authorizeNft;
+exports.createNft = createNft;
+exports.findWallet = findWallet;
 exports.connectToMaxonrowBlockchain = connectToMaxonrowBlockchain;
